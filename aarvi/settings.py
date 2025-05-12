11890e10327f0f32b277c6c0c8f3fc2b85215db2
sankalp <sankalpkashiv0@gmail.com>
2025-05-12 14:02:51 +0530

    changes

--- aarvi/settings.py
+++ aarvi/settings.py
@@ -54,11 +54,17 @@ REST_FRAMEWORK = {
     ),
 }
 
+CSRF_COOKIE_SECURE = False  
+CSRF_TRUSTED_ORIGINS = [
+    "https://aarvi-paints.onrender.com",
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
