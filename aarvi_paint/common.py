c225126215a0f133b094a5dc1905eb4fa80887e7
sankalp <sankalpkashiv0@gmail.com>
2025-05-16 13:09:26 +0530

    changes

--- aarvi_paint/common.py
+++ aarvi_paint/common.py
@@ -1,5 +1,6 @@
 # utils/user_info_handler.py
 
+import re
 from rest_framework.exceptions import ValidationError
 
 from .models import UserInfo
@@ -20,12 +21,30 @@ def validate_request_data(data, required_fields):
 
     return metadata
 
+
+
 def create_user_info(metadata, source):
+    name = metadata.get('name',"").strip()
+    phone_number = metadata.get('phone_number',"").strip()
+    email = metadata.get('email',"").strip()
+    pincode = metadata.get('pincode',"").strip()
+
+
+    if not re.match(r'^[6-9]\d{9}$', phone_number):
+            raise ValidationError("Mobile number must be 10 digits and start with 6, 7, 8, or 9.")
+    
+
+    if not re.match(r'^[a-zA-Z0-9._%+-]+@gmail\.com$', email):
+            raise ValidationError("Only valid Gmail addresses with a name before @gmail.com are allowed.")
+
+    if not (pincode.isdigit() and len(pincode) == 6):
+            raise ValidationError("The pin code should be exactly 6 digits.")
+    
     return UserInfo.objects.create(
-        name=metadata.get("name"),
-        phone_number=metadata.get("phone_number"),
-        email=metadata.get("email"),
-        pincode=metadata.get("pincode"),
+        name=name,
+        phone_number=phone_number,
+        email=email,
+        pincode=pincode,
         type=metadata.get("type"),
         description=metadata.get("description"),
         source=source
